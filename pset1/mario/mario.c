b3327f156998760b19bada8339fe2a765006940a
zer0_u <epsi10n7290@gmail.com>
2022-09-28 14:12:19 +0000

    Wed, Sep 28, 2022, 11:12 PM +09:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void print(int, string);
+void print_blocks(int);
+void print_spaces(int);
+
 int main(void)
 {
     int height = 0;
@@ -11,24 +15,33 @@ int main(void)
     while (height <= 0 || height > 8);
 
     int blocks = 1;
-    for(int i = 0;i < height;i++)
+    for (int i = 0; i < height; i++)
     {
 
-        int spaces = height-blocks;
-        for(int j = 0;j<spaces;j++)
-        {
-            printf(" ");
-        }
-        for(int j = 0;j<blocks;j++)
-        {
-            printf("#");
-        }
+        int spaces = height - blocks;
+        print_spaces(spaces);
+        print_blocks(blocks);
         printf("  ");
-        for(int j = 0;j<blocks;j++)
-        {
-            printf("#");
-        }
+        print_blocks(blocks);
         blocks++;
         printf("\n");
+    }
+}
+
+void print_blocks(int blocks)
+{
+    print(blocks, "#");
+}
+
+void print_spaces(int spaces)
+{
+    print(spaces, " ");
+}
+
+void print(int num, string type)
+{
+    for (int j = 0; j < num; j++)
+    {
+        printf(type);
     }
 }
