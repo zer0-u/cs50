8a5489336a7c1bb2d27430bd00b325b1badd93c6
zer0_u <epsi10n7290@gmail.com>
2022-09-28 13:22:04 +0000

    Wed, Sep 28, 2022, 10:22 PM +09:00

---
+++ pset1/population/population.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start = get_int("Start size: ");
+
+    // TODO: Prompt for end size
+    int end = get_int("End size: ");
+
+    // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+
+    // TODO: Print number of years
+    printf("Years: %i\n",years);
+}
