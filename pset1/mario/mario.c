2d806bc1ee86ee311c698e47ea53c8c647c678bb
zer0_u <epsi10n7290@gmail.com>
2022-09-28 13:54:09 +0000

    Wed, Sep 28, 2022, 10:54 PM +09:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -10,5 +10,14 @@ int main(void)
     }
     while (height < 0);
 
-
+    int width = 1;
+    for(int i = 0;i < height;i++)
+    {
+        for(int j =0; j<width;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        width++;
+    }
 }
