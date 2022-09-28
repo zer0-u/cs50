9a54a4d60cb2c4e6cc99b12e66d7a76ca0d5ae7c
zer0_u <epsi10n7290@gmail.com>
2022-09-28 13:39:23 +0000

    Wed, Sep 28, 2022, 10:39 PM +09:00

--- pset1/population/population.c
+++ pset1/population/population.c
@@ -4,10 +4,18 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    int start = get_int("Start size: ");
+    int start = 0;
+    do
+    {
+        start = get_int("Start size: ");
+    }while(start < 9);
 
     // TODO: Prompt for end size
     int end = get_int("End size: ");
+    do
+    {
+
+    }while(end =< start);
 
     // TODO: Calculate number of years until we reach threshold
     int years = 0;
