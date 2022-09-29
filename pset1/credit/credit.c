03fb437fb7531b898b930ef5dbc9c8323739c068
zer0_u <epsi10n7290@gmail.com>
2022-09-29 02:29:54 +0000

    Thu, Sep 29, 2022, 11:29 AM +09:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <stdlib.h>
 
 int main(void)
 {
@@ -11,9 +10,5 @@ int main(void)
     }
     while (number <= 0);
 
-    char num[];
-    sprintf(num,"%ld",number);
-    int size = sizeof(num)/sizeof(num[0]);
-    printf("%i\n", size);
-    printf("%c\n", num[1]);
+    
 }
