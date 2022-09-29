c6f432af95b1a68841157de89b710b1fbe843bff
zer0_u <epsi10n7290@gmail.com>
2022-09-29 02:40:03 +0000

    Thu, Sep 29, 2022, 11:40 AM +09:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -11,5 +11,8 @@ int main(void)
     while (number <= 0);
 
     int last_2 = (number % 100) / 10;
-    printf("%i\n", last_2);
+    int last_4 = (number % 10000) / 1000;
+    int last_6 = (number % 1000000) / 100000;
+    int last_8 = (number % 100000000) / 10000000;
+    printf("%i, %i, %i, %i\n",last_2, last_4, last_6, last_8);
 }
