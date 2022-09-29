cf922f319e11ff3970821ffc37e70e1180bb04d3
zer0_u <epsi10n7290@gmail.com>
2022-09-29 01:52:39 +0000

    Thu, Sep 29, 2022, 10:52 AM +09:00

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -2,32 +2,36 @@
 #include <cs50.h>
 #include <math.h>
 
-int remove_coin(int,int);
 
 int main(void)
 {
     float owed = get_float("Change owed : ");
     int cents = round(owed * 100);
 
-    int count_25 = remove_coin(cents, 25);
+    int count_25 = 0;
+    while(cents >= 25)
+    {
+        cents -= 25;
+        count_25++;
+    }
 
-    int count_10 = remove_coin(cents, 10);
+    int count_10 = 0;
+    while(cents >= 10)
+    {
+        cents -= 10;
+        count_10++;
+    }
 
-    int count_5 = remove_coin(cents, 5);
+    int count_5 = 0;
+    while(cents >= 5)
+    {
+        cents -= 5;
+        count_5++;
+    }
 
-    int count_1 = remove_coin(cents, 1);
+    int count_1 = cents;
     int count = count_25 + count_10 + count_5 + count_1;
     printf("25 * %i + 10 * %i + 5 * %i + %i\n", count_25, count_10, count_5, count_1);
     printf("%i\n",count);
 }
 
-int remove_coin(int cents, int coin)
-{
-    int count = 0;
-    while (cents >= coin)
-    {
-        cents -= coin;
-        count++;
-    }
-    return count;
-}
