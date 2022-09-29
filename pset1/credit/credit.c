67fb79a756c59e3fc0ef82005fffd774bfcc1397
zer0_u <epsi10n7290@gmail.com>
2022-09-29 02:43:18 +0000

    Thu, Sep 29, 2022, 11:43 AM +09:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -14,5 +14,9 @@ int main(void)
     int last_4 = (number % 10000) / 1000;
     int last_6 = (number % 1000000) / 100000;
     int last_8 = (number % 100000000) / 10000000;
-    printf("%i, %i, %i, %i\n",last_2, last_4, last_6, last_8);
+    int last_10 = (number % 10000000000) / 1000000000;
+    int last_12 = (number % 1000000000000) / 100000000000;
+    int last_14 = (number % 100000000000000) / 100000000000;
+    int last_16 = (number % 10000000000000000) / 10000000000000;
+    printf("%i, %i, %i, %i, %i, %i, %i, %i\n",last_2, last_4, last_6, last_8, last_10, last_12, last_14, last_16);
 }
