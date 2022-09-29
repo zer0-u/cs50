17b7b1084948026bfb13c646e753cfd3311d2d1a
zer0_u <epsi10n7290@gmail.com>
2022-09-29 01:30:47 +0000

    Thu, Sep 29, 2022, 10:30 AM +09:00

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -7,4 +7,11 @@ int main(void)
     float owed = get_float("Change owed : ");
     int cents = round(owed * 100);
     printf("cents : %i\n", cents);
+    int count_25 = 0;
+    while(cents >= 25)
+    {
+        cents -= 25;
+        count_25++;
+    }
+    printf("cents : %i\n", cents);
 }
