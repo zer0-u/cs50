e8e31d0d1deae0fc253387cb083d029a846cd690
zer0_u <epsi10n7290@gmail.com>
2022-11-08 07:30:23 +0000

    Tue, Nov 8, 2022, 7:30 AM +00:00

--- 2_Array/problem/readability/readability.c
+++ 2_Array/problem/readability/readability.c
@@ -99,6 +99,7 @@ int compute_coleman_liau(int letters, int words, int sentences)
     // wordsで割る＝1単語あたりの数を出して100倍するだけだった
     // wikipediaでちょっとカンニングした
     // https://en.wikipedia.org/wiki/Coleman%E2%80%93Liau_index
+    // wordsで割る時に既に切り捨てられていることにしばらく気付かなかった
     float l = (letters / (float)words) * 100;
     float s = (sentences / (float)words) * 100;
     int grade = round(0.0588 * l - 0.296 * s - 15.8);
