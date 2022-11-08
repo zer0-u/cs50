fdbc0e23151e75b69aea636c41d4b90194ce6808
zer0_u <epsi10n7290@gmail.com>
2022-11-08 04:16:08 +0000

    Tue, Nov 8, 2022, 4:16 AM +00:00

--- 2_Array/problem/readability/readability.c
+++ 2_Array/problem/readability/readability.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+
+int count_letters(string text);
 
 int main(void)
 {
     string input = get_string("Text: ");
+    int letters = count_letters(input);
+    printf("%i\n", letters);
+}
 
-    printf("%s\n", input);
+int count_letters(string text)
+{
+    int count = 0;
+    
+
+    return count;
 }
