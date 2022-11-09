19b5b4aa08b34d4de3ecb78b84988d19cdea8630
zer0_u <epsi10n7290@gmail.com>
2022-11-09 08:40:54 +0000

    Wed, Nov 9, 2022, 8:40 AM +00:00

--- 2_Array/problem/caesar/caesar.c
+++ 2_Array/problem/caesar/caesar.c
@@ -2,28 +2,63 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
+
+bool validate_args(int length, string args[]);
 bool key_is_digit(string input);
+void crypt(int key, string text);
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    // コマンドライン引数からkeyを取得する
+    if (!validate_args(argc, argv))
     {
-        printf("Usage: ./caesar key");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
-    string input_key = argv[1];
-    if (!key_is_digit(input_key))
+    int key = atoi(argv[1]);
+    if (key < 0)
     {
-        printf("Usage: ./caesar key");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
-
-    int key = atoi(input_key);
+    // TODO 後で消す
     printf("%i\n", key);
+
+    // 平文を受け取る
+    string plain = get_string("plaintext: ");
+
+    // 変換したものを出力する
+    crypt(key, plain);
+}
+
+void crypt(int key, string text)
+{
+    printf("ciphertext: ");
+    for (int i = 0, length = strlen(text); i < length; i++)
+    {
+        
+    }
+}
+
+// コマンドライン引数のチェック
+bool validate_args(int length, string args[])
+{
+    if (length != 2)
+    {
+        return false;
+    }
+    return key_is_digit(args[1]);
 }
 
 bool key_is_digit(string input)
 {
-    
-    return false;
+    for (int i = 0, length = strlen(input); i < length; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            return false;
+        }
+    }
+    return true;
 }
