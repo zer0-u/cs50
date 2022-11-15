16b361937be91529d140187e6fd8db8f9c3a079c
zer0_u <epsi10n7290@gmail.com>
2022-11-15 09:26:37 +0000

    Tue, Nov 15, 2022, 9:26 AM +00:00

--- 2_Array/problem/substitution/substitution.c
+++ 2_Array/problem/substitution/substitution.c
@@ -1,8 +1,24 @@
 // https://cs50.jp/x/2021/week2/problem-set/substitution-more-comfortable/
-// 2022/11/15 - 
+// 2022/11/15 -
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
+
+    if (argc != 2)
+    {
+        printf("ERROR\n");
+        return 1;
+    }
+}
+
+bool has_enough_length(string key[])
+{
+    if (strlen(key) != 26)
+    {
+        return false;
+    }
+    return false;
 }
