8628035acc04c043de7f8b373bc1eaef4c69ca84
zer0_u <epsi10n7290@gmail.com>
2022-11-23 08:43:42 +0000

    Wed, Nov 23, 2022, 8:43 AM +00:00

--- 3_Algorithms/lecture/names.c
+++ 3_Algorithms/lecture/names.c
@@ -5,5 +5,15 @@
 int main(void)
 {
     string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
-    
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (strcmp(names[i], "Ron") == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not Found\n");
+    return 1;
 }
