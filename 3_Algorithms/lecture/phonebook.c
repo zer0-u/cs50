e81461e0048c1281a77552f26ab8f20289875ee0
zer0_u <epsi10n7290@gmail.com>
2022-11-23 09:53:14 +0000

    Wed, Nov 23, 2022, 9:53 AM +00:00

--- 3_Algorithms/lecture/phonebook.c
+++ 3_Algorithms/lecture/phonebook.c
@@ -6,4 +6,15 @@ int main(void)
 {
     string names[] = {"Brian", "David"};
     string numbers[] = {"000-000", "111-111"};
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], "David") == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not Found\n");
+    return 1;
 }
