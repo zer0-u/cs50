66a4efd544a3e87a2179af8b36fd7563e3108b88
zer0_u <epsi10n7290@gmail.com>
2022-12-03 07:57:21 +0000

    Sat, Dec 3, 2022, 7:57 AM +00:00

--- 3_Algorithms/problem/runoff/runoff.c
+++ 3_Algorithms/problem/runoff/runoff.c
@@ -130,8 +130,10 @@ bool vote(int voter, int rank, string name)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        string name = candidates[i].name;
-        
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            preferences[voter][rank] = name;
+        }
     }
     return false;
 }
