efc063e55ff9e40af2845727d12eae7f7f02d40e
zer0_u <epsi10n7290@gmail.com>
2022-12-11 04:42:03 +0000

    Sun, Dec 11, 2022, 4:42 AM +00:00

--- 3_Algorithms/problem/tideman/tideman.c
+++ 3_Algorithms/problem/tideman/tideman.c
@@ -101,7 +101,7 @@ bool vote(int rank, string name, int ranks[])
     for (int i = 0; i < candidate_count; i++)
     {
         if(strcmp(name,candidates[i])===0){
-            
+            ranks[rank] = i;
         }
     }
     return false;
