0c2db90a489526a7f2849c82a353216d07cfd16a
zer0_u <epsi10n7290@gmail.com>
2022-12-11 08:25:03 +0000

    Sun, Dec 11, 2022, 8:25 AM +00:00

--- 3_Algorithms/problem/tideman/tideman.c
+++ 3_Algorithms/problem/tideman/tideman.c
@@ -157,9 +157,11 @@ void sort_pairs(void)
 {
     for (int i = 0; i < pair_count; i++)
     {
-        
         for (int j = i + 1; j < pair_count; j++)
         {
+            pair p1 = pairs[i];
+            pair p2 = pairs[j];
+            
 
         }
     }
