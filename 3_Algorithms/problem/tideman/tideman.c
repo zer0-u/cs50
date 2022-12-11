45da54c68d58058c3d6cb27a3306f758636f4077
zer0_u <epsi10n7290@gmail.com>
2022-12-11 10:19:51 +0000

    Sun, Dec 11, 2022, 10:19 AM +00:00

--- 3_Algorithms/problem/tideman/tideman.c
+++ 3_Algorithms/problem/tideman/tideman.c
@@ -176,7 +176,11 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
+
+    for (int i = 0; i < pair_count; i++)
+    {
+        
+    }
     return;
 }
 
