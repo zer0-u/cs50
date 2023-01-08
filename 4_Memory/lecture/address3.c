378e6df4c6e1b3997375d33d94ecaf17ebfacd2e
zer0_u <epsi10n7290@gmail.com>
2023-01-08 08:18:35 +0000

    Sun, Jan 8, 2023, 8:18 AM +00:00

--- 4_Memory/lecture/address3.c
+++ 4_Memory/lecture/address3.c
@@ -1,12 +1,11 @@
 // 2023/01/08
 #include <stdio.h>
-#include <cs50.h>
+// #include <cs50.h>
 
 int main(void)
 {
-    string s = "HI!";
+    char *s = "HI!";
     printf("%c\n", s[0]);
     printf("%c\n", s[1]);
     printf("%c\n", s[2]);
-    printf("%c\n", s[3]);
 }
