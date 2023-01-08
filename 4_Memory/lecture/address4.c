cf4e580280c015cd4d722321f8c728efbaef8cdd
zer0_u <epsi10n7290@gmail.com>
2023-01-08 08:30:54 +0000

    Sun, Jan 8, 2023, 8:30 AM +00:00

---
+++ 4_Memory/lecture/address4.c
@@ -0,0 +1,11 @@
+// 2023/01/08
+#include <stdio.h>
+// #include <cs50.h>
+
+int main(void)
+{
+    int numbers[] = {4, 6, 8, 2, 7, 5, 0};
+    printf("%i\n", numbers[0]);
+    printf("%i\n", numbers[1]);
+    printf("%i\n", numbers[2]);
+}
