27562c02a506c5144a9bc21697100c6a83a67e6b
zer0_u <epsi10n7290@gmail.com>
2023-01-08 09:49:07 +0000

    Sun, Jan 8, 2023, 9:49 AM +00:00

--- 4_Memory/lecture/copy.c
+++ 4_Memory/lecture/copy.c
@@ -6,8 +6,15 @@
 
 int main(void)
 {
-    string s = get_string("s: ");
-    string t = s;
+    char *s = get_string("s: ");
+
+    char *t = malloc(strlen(s) + 1);
+
+    for (int i = 0, n = strlen(s) + 1; i < n; i++)
+    {
+        t[i] = s[i];
+    }
+
     t[0] = toupper(t[0]);
 
     printf("s: %s\n", s);
