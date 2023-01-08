af7d735f0c56bd06cb32c3204770c5435f56c084
zer0_u <epsi10n7290@gmail.com>
2023-01-08 08:54:49 +0000

    Sun, Jan 8, 2023, 8:54 AM +00:00

--- 4_Memory/lecture/compare.c
+++ 4_Memory/lecture/compare.c
@@ -1,12 +1,13 @@
 // 2023/01/08
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
     char *t = get_string("t: ");
-    if (s == t)
+    if (strcmp(s, t) == 0)
     {
         printf("Same\n");
     }
