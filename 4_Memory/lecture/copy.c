bb84d481c63f6377c99fb4cc86e99dd29d4034c6
zer0_u <epsi10n7290@gmail.com>
2023-01-08 10:00:08 +0000

    Sun, Jan 8, 2023, 10:00 AM +00:00

--- 4_Memory/lecture/copy.c
+++ 4_Memory/lecture/copy.c
@@ -3,6 +3,7 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -10,10 +11,12 @@ int main(void)
 
     char *t = malloc(strlen(s) + 1);
 
-    for (int i = 0, n = strlen(s) + 1; i < n; i++)
-    {
-        t[i] = s[i];
-    }
+    // for (int i = 0, n = strlen(s) + 1; i < n; i++)
+    // {
+    //     t[i] = s[i];
+    // }
+
+    strcpy(t, s);
 
     t[0] = toupper(t[0]);
 
