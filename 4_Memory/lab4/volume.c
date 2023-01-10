0c2169028108571cee25223b4252e57eeb3b129e
zer0_u <epsi10n7290@gmail.com>
2023-01-10 12:32:40 +0000

    Tue, Jan 10, 2023, 12:32 PM +00:00

--- 4_Memory/lab4/volume.c
+++ 4_Memory/lab4/volume.c
@@ -34,6 +34,8 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+
 
     // TODO: Read samples from input file and write updated data to output file
 
