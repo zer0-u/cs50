864b5a29225a81cb0ebb0d600eaf7f7cf1f1a8d8
zer0_u <epsi10n7290@gmail.com>
2023-01-10 12:48:21 +0000

    Tue, Jan 10, 2023, 12:48 PM +00:00

--- 4_Memory/lab4/volume.c
+++ 4_Memory/lab4/volume.c
@@ -36,9 +36,9 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     uint8_t h;
-    while (fread(&h, sizeof(uint_8), HEADER_SIZE, input))
+    while (fread(&h, sizeof(uint8_t), HEADER_SIZE, input))
     {
-        fwrite(&h, sizeof(uint_8), HEADER_SIZE, output)
+        fwrite(&h, sizeof(uint8_t), HEADER_SIZE, output);
     }
 
     // TODO: Read samples from input file and write updated data to output file
