f4cff40aaf6f69c200549496ecc3033834b48cf8
zer0_u <epsi10n7290@gmail.com>
2023-01-10 09:50:50 +0000

    Tue, Jan 10, 2023, 9:50 AM +00:00

--- 4_Memory/lecture/swap.c
+++ 4_Memory/lecture/swap.c
@@ -1,7 +1,7 @@
 // 2023/01/08
 #include <stdio.h>
 
-void swap(int a, int b);
+void swap(int *a, int *b);
 
 int main(void)
 {
@@ -9,15 +9,13 @@ int main(void)
     int y = 2;
 
     printf("x is %i, y is %i\n", x, y);
-    swap(x, y);
+    swap(&x, &y);
     printf("x is %i, y is %i\n", x, y);
 }
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-    printf("a is %i, b is %i\n", a, b);
-    int tmp = a;
-    a = b;
-    b = tmp;
-    printf("a is %i, b is %i\n", a, b);
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
