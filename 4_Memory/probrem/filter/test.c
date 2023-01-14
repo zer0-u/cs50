1095372682513c0425971fd0bc9d28d4563d813a
zer0_u <epsi10n7290@gmail.com>
2023-01-14 08:14:13 +0000

    Sat, Jan 14, 2023, 8:14 AM +00:00

--- 4_Memory/probrem/filter/test.c
+++ 4_Memory/probrem/filter/test.c
@@ -6,13 +6,47 @@
 
 int main(int argc, char *argv[])
 {
+    // (編集) テストしたい関数の種類
+    char filter = 'b';
 
+    // (編集) テストする画像の高さと幅
     int height = 0;
     int width = 0;
 
     RGBTRIPLE(*image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
-    
 
+    // imageに値を詰める
+
+    // image[0][0]
+
+    // テスト対象の関数を呼び出す
+    switch (filter)
+    {
+    // Blur
+    case 'b':
+        blur(height, width, image);
+        break;
+
+    // Grayscale
+    case 'g':
+        grayscale(height, width, image);
+        break;
+
+    // Reflection
+    case 'r':
+        reflect(height, width, image);
+        break;
+
+    // Sepia
+    case 's':
+        sepia(height, width, image);
+        break;
+    }
+
+    // imageが想定通りか出力する
+
+
+    // 後片付け
     free(image);
     return 0;
 }
