72c9a75cbaee2dec2ff5cf7960025360b1cc4fe4
zer0_u <epsi10n7290@gmail.com>
2023-01-14 08:29:16 +0000

    Sat, Jan 14, 2023, 8:29 AM +00:00

--- 4_Memory/probrem/filter/test.c
+++ 4_Memory/probrem/filter/test.c
@@ -14,14 +14,48 @@ int main(int argc, char *argv[])
     int height = 3;
     int width = 3;
 
+    // (固定) image配列用のメモリを確保する
     RGBTRIPLE(*image)
     [width] = calloc(height, width * sizeof(RGBTRIPLE));
 
-    // imageに値を詰める
+    // (編集)imageに値を詰める
+    image[0][0].rgbtBlue = 10;
+    image[0][0].rgbtGreen = 20;
+    image[0][0].rgbtRed = 30;
 
-    image[0][0] = {rgbtBlue=}
+    image[0][1].rgbtBlue = 40;
+    image[0][1].rgbtGreen = 50;
+    image[0][1].rgbtRed = 60;
 
-    // テスト対象の関数を呼び出す
+    image[0][2].rgbtBlue = 70;
+    image[0][2].rgbtGreen = 80;
+    image[0][2].rgbtRed = 90;
+
+    image[1][0].rgbtBlue = 110;
+    image[1][0].rgbtGreen = 130;
+    image[1][0].rgbtRed = 140;
+
+    image[1][1].rgbtBlue = 120;
+    image[1][1].rgbtGreen = 140;
+    image[1][1].rgbtRed = 150;
+
+    image[1][2].rgbtBlue = 130;
+    image[1][2].rgbtGreen = 150;
+    image[1][2].rgbtRed = 160;
+
+    image[2][0].rgbtBlue = 200;
+    image[2][0].rgbtGreen = 210;
+    image[2][0].rgbtRed = 220;
+
+    image[2][1].rgbtBlue = 220;
+    image[2][1].rgbtGreen = 230;
+    image[2][1].rgbtRed = 240;
+
+    image[2][2].rgbtBlue = 240;
+    image[2][2].rgbtGreen = 250;
+    image[2][2].rgbtRed = 255;
+
+    // (固定) テスト対象の関数を呼び出す
     switch (filter)
     {
     // Blur
@@ -45,7 +79,7 @@ int main(int argc, char *argv[])
         break;
     }
 
-    // imageが想定通りか出力する
+    // (固定) imageが想定通りか出力する
     for (int h = 0; h < height; h++)
     {
         for (int w = 0; w < width; w++)
@@ -59,7 +93,7 @@ int main(int argc, char *argv[])
         printf("\n");
     }
 
-    // 後片付け
+    // (固定) 後片付け
     free(image);
     printf("test end\n");
     return 0;
