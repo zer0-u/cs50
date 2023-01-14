21d22968f71b05787c09091f93e998f8479bd9bf
zer0_u <epsi10n7290@gmail.com>
2023-01-14 05:56:53 +0000

    Sat, Jan 14, 2023, 5:56 AM +00:00

--- 4_Memory/probrem/filter/helpers.c
+++ 4_Memory/probrem/filter/helpers.c
@@ -17,13 +17,24 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[h][w].rgbtRed = average;
         }
     }
-    return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            RGBTRIPLE pixel = image[h][w];
+            BYTE blue = pixel.rgbtBlue;
+            BYTE green = pixel.rgbtGreen;
+            BYTE red = pixel.rgbtRed;
+            BYTE sepiaRed = round(0.393 * red + 0.769 * green + 0.189 * blue);
+            BYTE sepiaGreen = round(0.349 * red + 0.686 * green + 0.168 * blue);
+            BYTE sepiaBlue = round(0.272 * red + 0.534 * green + 0.131 * blue);
+        }
+    }
 }
 
 // Reflect image horizontally
