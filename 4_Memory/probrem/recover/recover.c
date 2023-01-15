7d4e72f936cff642a6e43869689c7d070b70662f
zer0_u <epsi10n7290@gmail.com>
2023-01-15 06:39:42 +0000

    Sun, Jan 15, 2023, 6:39 AM +00:00

--- 4_Memory/probrem/recover/recover.c
+++ 4_Memory/probrem/recover/recover.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // 引数チェック
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
 }
