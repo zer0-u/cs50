fcb5ed529eed4dc7b529229801e0123e6188d492
zer0_u <epsi10n7290@gmail.com>
2023-01-15 09:01:02 +0000

    Sun, Jan 15, 2023, 9:01 AM +00:00

--- 4_Memory/probrem/recover/recover.c
+++ 4_Memory/probrem/recover/recover.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
                 fclose(output);
             }
             // 新しいファイルを開く
-            sprintf(filename, "%03i.jpg", filenum);
+            sprintf(filename, "%03i.txt", filenum);
             filenum++;
             output = fopen(filename, "w");
             if (output == NULL)
@@ -52,11 +52,7 @@ int main(int argc, char *argv[])
         }
         else if (filenum != 0) // ファイルが開いている場合は書き込む
         {
-            for (int i = 0; i < BLOCK_SIZE; i++)
-            {
-                printf("%i", i);
-            }
-            printf("\n");
+
             fwrite(block, BLOCK_SIZE, 1, output);
         }
     }
