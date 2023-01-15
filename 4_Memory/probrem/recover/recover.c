bca4aea0e2aadbbd47a2fab7fe3edf424e6ceaa6
zer0_u <epsi10n7290@gmail.com>
2023-01-15 06:43:01 +0000

    Sun, Jan 15, 2023, 6:43 AM +00:00

--- 4_Memory/probrem/recover/recover.c
+++ 4_Memory/probrem/recover/recover.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int BLOCK_SIZE = 512;
+
 int main(int argc, char *argv[])
 {
     // 引数チェック
@@ -11,11 +13,19 @@ int main(int argc, char *argv[])
     }
 
     // メモリーカードを開く
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
+    FILE *memory = fopen(argv[1], "r");
+    if (memory == NULL)
     {
         printf("Could not open file.\n");
         return 1;
+    }
+
+    byte b;
+
+    while (fread(&b, int16size, 1, input))
+    {
+        b = b * factor;
+        fwrite(&b, int16size, 1, output);
     }
 
 }
