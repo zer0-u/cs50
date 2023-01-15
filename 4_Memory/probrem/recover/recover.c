5131b64b5151cd8510034259ecf35ace06db11a4
zer0_u <epsi10n7290@gmail.com>
2023-01-15 07:10:20 +0000

    Sun, Jan 15, 2023, 7:10 AM +00:00

--- 4_Memory/probrem/recover/recover.c
+++ 4_Memory/probrem/recover/recover.c
@@ -6,28 +6,27 @@ const int BLOCK_SIZE = 512;
 int main(int argc, char *argv[])
 {
     // 引数チェック
-    // if (argc != 2)
-    // {
-    //     printf("Usage: ./recover image\n");
-    //     return 1;
-    // }
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
 
     // メモリーカードを開く
-    // FILE *memory = fopen(argv[1], "r");
-    // if (memory == NULL)
-    // {
-    //     printf("Could not open file.\n");
-    //     return 1;
-    // }
+    FILE *memory = fopen(argv[1], "r");
+    if (memory == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
 
-    uint8_t aaa[BLOCK_SIZE];
-    // uint8_t block[BLOCK_SIZE];
+    int block[BLOCK_SIZE];
 
-    // while (fread(block, BLOCK_SIZE, 1, input))
-    // {
+    while (fread(block, BLOCK_SIZE, 1, memory))
+    {
 
-    // }
+    }
 
     // 後片付け
-    // fclose(memory);
+    fclose(memory);
 }
