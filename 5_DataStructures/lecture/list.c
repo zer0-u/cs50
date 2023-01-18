80041236a02a62127c240337e26d4861e0c86ae2
zer0_u <epsi10n7290@gmail.com>
2023-01-18 11:32:21 +0000

    Wed, Jan 18, 2023, 11:32 AM +00:00

--- 5_DataStructures/lecture/list.c
+++ 5_DataStructures/lecture/list.c
@@ -5,7 +5,7 @@
 int main(void)
 {
     // ヒープ領域に確保する
-    int *list = malloc(8 * sizeof(int));
+    int *list = malloc(3 * sizeof(int));
     if (list == null)
     {
         return 1;
@@ -14,6 +14,11 @@ int main(void)
     list[0] = 1; // *list = 1;
     list[1] = 2; // *(list + 1) = 2;
     list[2] = 3; // *(list + 2) = 3;
+
+    // 配列の要素数を増やす
+    list = malloc(4 * sizeof(int));
+
+    list[3] = 4;
 
     free(list);
 }
