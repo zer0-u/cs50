33faa5a810f92cfbaf312a939bc4ed75d08b6e56
zer0_u <epsi10n7290@gmail.com>
2023-01-18 11:41:33 +0000

    Wed, Jan 18, 2023, 11:41 AM +00:00

--- 5_DataStructures/lecture/list.c
+++ 5_DataStructures/lecture/list.c
@@ -16,11 +16,34 @@ int main(void)
     list[2] = 3; // *(list + 2) = 3;
 
     // 配列の要素数を増やしたいので領域を確保し直す
-    list = malloc(4 * sizeof(int));
+    // list = malloc(4 * sizeof(int));
     // ↑だと8行目で確保したメモリへのポインタが失われ、
     // 0~2番目の要素も参照できなくなってしまう
 
-    list[3] = 4;
+    // 別の変数にポインタを代入する
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == null)
+    {
+        free(list);
+        return 1;
+    }
 
+    // 4つの要素を持てる配列tmpに配列listの要素をコピーする
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+    // 要素を追加する
+    tmp[3] = 4;
+
+    // ポインタを代入し直す
+    list = tmp;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", list[i]);
+    }
     free(list);
+    free(tmp);
+    return 0;
 }
