3146bb963b6943df9999a64b080097efe4ec9dc5
zer0_u <epsi10n7290@gmail.com>
2023-01-18 11:29:42 +0000

    Wed, Jan 18, 2023, 11:29 AM +00:00

--- 5_DataStructures/lecture/list.c
+++ 5_DataStructures/lecture/list.c
@@ -6,6 +6,14 @@ int main(void)
 {
     // ヒープ領域に確保する
     int *list = malloc(8 * sizeof(int));
+    if (list == null)
+    {
+        return 1;
+    }
+
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
 
     free(list);
 }
