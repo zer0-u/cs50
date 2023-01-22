fbaac0ef2f0266462805ddea04c4bd12230f194b
zer0_u <epsi10n7290@gmail.com>
2023-01-22 08:32:18 +0000

    Sun, Jan 22, 2023, 8:32 AM +00:00

--- 5_DataStructures/lecture/tree.c
+++ 5_DataStructures/lecture/tree.c
@@ -14,24 +14,42 @@ void print_tree(node *root);
 
 int main(void)
 {
+    // 木構造そのものを保持する変数を宣言
     node *tree = NULL;
+
+    // 1つのノードを生成・初期化
     node *n = malloc(sizeof(node));
     if (n == NULL)
     {
         return 1;
     }
-
     n->number = 2;
     n->left = NULL;
     n->right = NULL;
+
+    // 根としてノードを保持する
     tree = n;
 
+    // 新しいノードを生成・初期化
     n = malloc(sizeof(node));
     if (n == NULL)
     {
         return 1;
     }
     n->number = 1;
+    n->left = NULL;
+    n->right = NULL;
+    tree->left = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->number = 3;
+    n->left = NULL;
+    n->right = NULL;
+    tree->right = n;
 }
 
 void free_tree(node *root)
