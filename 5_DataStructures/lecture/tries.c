9431b1256a08f4224d7707d91db3af9eb3b22473
zer0_u <epsi10n7290@gmail.com>
2023-01-22 11:34:24 +0000

    Sun, Jan 22, 2023, 11:34 AM +00:00

--- 5_DataStructures/lecture/tries.c
+++ 5_DataStructures/lecture/tries.c
@@ -1,12 +1,14 @@
 // 2023/01/22
 #include <stdio.h>
-#include <cs50.h>
+#include <stdbool.h>
 
 const int SIZE_OF_ALPHABET = 26;
 
 typedef struct node
 {
+    // 終端の場合はtrue
     bool is_word;
+    // それぞれのノードは最大で26個の子を持つ
     struct node *children[SIZE_OF_ALPHABET];
 } node;
 
