9f3eab01e05ce2f614c415567fd7cefb4cfcaf38
zer0_u <epsi10n7290@gmail.com>
2023-01-22 10:11:39 +0000

    Sun, Jan 22, 2023, 10:11 AM +00:00

---
+++ 5_DataStructures/lecture/hashtables.c
@@ -0,0 +1,21 @@
+// 2023/01/22
+#include <stdio.h>
+#include <cs50.h>
+
+// 例
+const int LONGEST_WORD = 100;
+
+typedef struct node
+{
+    // 納めたい値そのもの
+    // stringはないので適当な長さのchar型の配列を用意する
+    // LONGEST_WORDは「これだけあれば多分大丈夫だろう」という字数
+    // +1はヌル文字の分
+    char word[LONGEST_WORD + 1];
+    struct node *next;
+}
+
+int
+main(void)
+{
+}
