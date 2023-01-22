490bde1e21590f0fe31ee1e9d8c52aa54a0ed6ba
zer0_u <epsi10n7290@gmail.com>
2023-01-22 08:29:01 +0000

    Sun, Jan 22, 2023, 8:29 AM +00:00

--- 5_DataStructures/lecture/tree.c
+++ 5_DataStructures/lecture/tree.c
@@ -1,7 +1,31 @@
 // 2023/01/22
 #include <stdio.h>
-#include <cs50.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+} node;
+
+void free_tree(node *root);
+void print_tree(node *root);
 
 int main(void)
+{
+    node *tree = NULL;
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+}
+
+void free_tree(node *root)
+{
+}
+
+void print_tree(node *root)
 {
 }
