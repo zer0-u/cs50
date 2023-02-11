296671fae29972b677eab601569b72b96584e7e7
zer0_u <epsi10n7290@gmail.com>
2023-02-11 08:52:03 +0000

    Sat, Feb 11, 2023, 8:52 AM +00:00

--- 5_DataStructures/pset/speller/dictionary.c
+++ 5_DataStructures/pset/speller/dictionary.c
@@ -9,8 +9,7 @@ typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
-}
-node;
+} node;
 
 // Number of buckets in hash table
 const unsigned int N = 1;
@@ -18,8 +17,8 @@ const unsigned int N = 1;
 // Hash table
 node *table[N];
 
-// Returns true if word is in dictionary, else false
-bool check(const char *word)
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
 {
     // TODO
     return false;
@@ -32,18 +31,18 @@ unsigned int hash(const char *word)
     return 0;
 }
 
-// Loads dictionary into memory, returning true if successful, else false
-bool load(const char *dictionary)
-{
-    // TODO
-    return false;
-}
-
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
     return 0;
+}
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
