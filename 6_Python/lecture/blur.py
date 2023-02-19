be3f33a0034e7e002de05491eab00c5ba3ea3058
zer0_u <epsi10n7290@gmail.com>
2023-02-19 14:08:21 +0000

    Sun, Feb 19, 2023, 2:08 PM +00:00

--- 6_Python/lecture/blur.py
+++ 6_Python/lecture/blur.py
@@ -1,6 +1,6 @@
 # 2023/02/19
 from PIL import Image, ImageFilter
 
-before = Image.open("bridge.bmp")
+before = Image.open("stadium.bmp")
 after = before.filter(ImageFilter.BoxBlur(1))
 after.save("out.bmp")
