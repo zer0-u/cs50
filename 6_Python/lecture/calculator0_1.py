a9f9f9d04fe08745d4f513db0213e44b46c8fa11
zer0_u <epsi10n7290@gmail.com>
2023-02-20 14:17:12 +0000

    Mon, Feb 20, 2023, 2:17 PM +00:00

--- 6_Python/lecture/calculator0_1.py
+++ 6_Python/lecture/calculator0_1.py
@@ -1,4 +1,12 @@
 
-x = int(input("x: "))
-y = int(input("y: "))
+try:
+    x = int(input("x: "))
+except:
+    print("that is not an int")
+    exit()
+try:
+    y = int(input("y: "))
+except:
+    print("that is not an int")
+    exit()
 print(x + y)
