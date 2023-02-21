95caf2216bbdf42fb6df3939f2781c98ecfc7f37
zer0_u <epsi10n7290@gmail.com>
2023-02-21 13:41:58 +0000

    Tue, Feb 21, 2023, 1:41 PM +00:00

--- 6_Python/lecture/agree.py
+++ 6_Python/lecture/agree.py
@@ -1,8 +1,8 @@
 from cs50 import get_string
 
-s = get_string("Do you agree? ")
+s = get_string("Do you agree? ").lower()
 
-if s.lower() in ["y", "yes"]:
+if s in ["y", "yes"]:
     print("Agreed.")
-elif s.lower() in ["n", "no"]:
+elif s in ["n", "no"]:
     print("Not Agreed.")
