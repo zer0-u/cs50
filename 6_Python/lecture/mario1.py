9c3714e8bf0a6d475391d6f9a403a3d32ea42013
zer0_u <epsi10n7290@gmail.com>
2023-02-21 14:24:03 +0000

    Tue, Feb 21, 2023, 2:24 PM +00:00

--- 6_Python/lecture/mario1.py
+++ 6_Python/lecture/mario1.py
@@ -1,9 +1,17 @@
 from cs50 import get_int
 
-while True:
-    n = get_int("Height: ")
-    if n > 0:
-        break
 
-for i in range(n):
-    print("#")
+def main():
+    for i in range(n):
+        print("#")
+
+
+def get_height():
+    while True:
+        n = get_int("Height: ")
+        if n > 0:
+            break
+    return n
+
+
+main()
