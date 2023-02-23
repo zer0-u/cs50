619f31a1cdee33dc5a283422b5d98db169cc2576
zer0_u <epsi10n7290@gmail.com>
2023-02-23 06:20:57 +0000

    Thu, Feb 23, 2023, 6:20 AM +00:00

--- 6_Python/lecture/temp/hogwarts.py
+++ 6_Python/lecture/temp/hogwarts.py
@@ -7,12 +7,19 @@ houses = {
     "S": 0
 }
 
+# with open("hogwarts.csv", "r") as file:
+#     # reader = csv.reader(file)
+#     # next(reader)  # ヘッダ行を無視する
+#     for row in reader:
+#         house = row[1]  # カンマを手がかりに要素へ分解してある
+#         houses[house] += 1
+
 with open("hogwarts.csv", "r") as file:
-    reader = csv.reader(file)
-    next(reader)  # ヘッダ行を無視する
+    reader = csv.DictReader(file)  # ヘッダ行をキーの名前として利用する
     for row in reader:
-        house = row[1]  # カンマを手がかりに要素へ分解してある
+        house = row["House"]  # キー名で参照できる
         houses[house] += 1
+
 
 for house in houses:
     count = houses[house]
