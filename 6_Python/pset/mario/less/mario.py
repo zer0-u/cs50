9d468ec28c1e2207cc49a6d7849fa1e2979ba6d7
zer0_u <epsi10n7290@gmail.com>
2023-03-01 11:47:13 +0000

    Wed, Mar 1, 2023, 11:47 AM +00:00

--- 6_Python/pset/mario/less/mario.py
+++ 6_Python/pset/mario/less/mario.py
@@ -4,7 +4,6 @@ while True:
     height = get_int("Height: ")
     if height in range(0, 9):
         break
-    print("input integer(0-8)")
 
 for i in range(1, height+1):
     print(" "*(height-i), end="")
