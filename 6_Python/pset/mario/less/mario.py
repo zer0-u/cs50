778e122f00fc4bb78045deaff42ce6ea85b0c18e
zer0_u <epsi10n7290@gmail.com>
2023-03-01 11:44:55 +0000

    Wed, Mar 1, 2023, 11:44 AM +00:00

--- 6_Python/pset/mario/less/mario.py
+++ 6_Python/pset/mario/less/mario.py
@@ -1,6 +1,11 @@
 from cs50 import get_int
 
-height = get_int("Height: ")
+while True:
+    height = get_int("Height: ")
+    if height in range(0, 9):
+        break
+    print("input integer(0-8)")
+
 for i in range(1, height+1):
     print(" "*(height-i), end="")
     print("#"*(i), end="")
