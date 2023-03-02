54cda5fce4b52abdaf7f892101752021f46c7c2e
zer0_u <epsi10n7290@gmail.com>
2023-03-02 12:47:46 +0000

    Thu, Mar 2, 2023, 12:47 PM +00:00

--- 6_Python/pset/cash/cash.py
+++ 6_Python/pset/cash/cash.py
@@ -1,17 +1,44 @@
 from cs50 import get_float
 
-while True:
-    input = get_float("Change owed: ")
-    if input >= 0:
-        break
 
-# セントに直す
-input *=100
+def main():
+    while True:
+        input = get_float("Change owed: ")
+        if input >= 0:
+            break
 
-# quarter(25)
+    # セントに直す
+    input *= 100
 
-# dime(10)
+    count = 0
+    # quarter(25)
+    result = calc(input, 25)
+    input = result["remain"]
+    count += result["count"]
 
-# nickel(5)
+    # dime(10)
+    result = calc(input, 10)
+    input = result["remain"]
+    count += result["count"]
 
-# penny(1)
+    # nickel(5)
+    result = calc(input, 5)
+    input = result["remain"]
+    count += result["count"]
+
+    # penny(1)
+    result = calc(input, 1)
+    input = result["remain"]
+    count += result["count"]
+    print(count)
+
+
+def calc(remain, coin):
+    count = 0
+    while remain > coin:
+        count += 1
+        remain -= coin
+    return {remain: remain, count: count}
+
+
+main()
