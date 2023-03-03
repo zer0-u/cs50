dde68e9134dd961be4d77f69932694c38fbcd3bb
zer0_u <epsi10n7290@gmail.com>
2023-03-03 12:46:52 +0000

    Fri, Mar 3, 2023, 12:46 PM +00:00

--- 6_Python/pset/credit/credit.py
+++ 6_Python/pset/credit/credit.py
@@ -3,13 +3,19 @@ from cs50 import get_string
 
 def main():
     number = get_string("Number :" )
+    check_header(number)
     if not check_length(number):
         print('INVALID')
         return
 
 
 
+
 def check_length(number):
     return len(number) in [13,15,16]
+
+def check_header(number):
+    header = number[0]
+    print(header)
 
 main()
