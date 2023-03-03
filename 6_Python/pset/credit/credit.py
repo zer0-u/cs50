55080e1a939e939225c63a53009d052a7bf76896
zer0_u <epsi10n7290@gmail.com>
2023-03-03 14:06:53 +0000

    Fri, Mar 3, 2023, 2:06 PM +00:00

--- 6_Python/pset/credit/credit.py
+++ 6_Python/pset/credit/credit.py
@@ -9,28 +9,18 @@ def main():
     if not is_valid(number):
         print('INVALID')
         return
-    
+    print(check_header(number))
 
 
 def check_length(number):
     return len(number) in [13, 15, 16]
 
 
-def check_header(number):
-    header = number[:2]
-    if header in ['34', '37']:
-        return True
-    elif header in ['51', '52', '53', '54', '55']:
-        return True
-    elif header[0] == '4':
-        return True
-    return False
-
-
 def is_valid(number):
     sum = 0
     is_even = False
     for i in range(len(number), 0, -1):
+        print(f'i: {i}, is_even: {is_even}')
         if is_even:
             elem = i*2
             if elem > 10:
@@ -39,10 +29,21 @@ def is_valid(number):
             else:
                 sum += elem
         else:
-            odd_sum += i
+            sum += i
         is_even = not is_even
 
     return sum % 10 == 0
+
+
+def check_header(number):
+    header = number[:2]
+    if header in ['34', '37']:
+        return 'AMEX'
+    elif header in ['51', '52', '53', '54', '55']:
+        return 'MASTERCARD'
+    elif header[0] == '4':
+        return 'VISA'
+    return 'INVALID'
 
 
 main()
