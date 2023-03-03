d681fa52f3b42654d2b30808d88adc45bce039f2
zer0_u <epsi10n7290@gmail.com>
2023-03-03 12:54:40 +0000

    Fri, Mar 3, 2023, 12:54 PM +00:00

--- 6_Python/pset/credit/credit.py
+++ 6_Python/pset/credit/credit.py
@@ -1,21 +1,23 @@
 from cs50 import get_string
+import re
 
 
 def main():
-    number = get_string("Number :" )
+    number = get_string("Number :")
     check_header(number)
     if not check_length(number):
         print('INVALID')
         return
 
 
-
-
 def check_length(number):
-    return len(number) in [13,15,16]
+    return len(number) in [13, 15, 16]
+
 
 def check_header(number):
     header = number[:2]
-    print(header)
+    pattern = ''
+    return re.match(pattern, header)
+
 
 main()
