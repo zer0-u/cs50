8d11e9de902f54bc2ed24338bff60a013f1eca79
zer0_u <epsi10n7290@gmail.com>
2023-03-03 12:30:44 +0000

    Fri, Mar 3, 2023, 12:30 PM +00:00

--- 6_Python/pset/credit/credit.py
+++ 6_Python/pset/credit/credit.py
@@ -1,7 +1,13 @@
 from cs50 import get_string
+import re
 
 def main():
     number = get_string("Number :" )
-    
+    pattern = '^\d'
+    result = re.match(pattern,number)
+    if result:
+        print(f'{number} matched')
+    else:
+        print(f'{number} not matched')
 
 main()
