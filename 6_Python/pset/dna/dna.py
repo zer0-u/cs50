8273b18de3c82fb04068684ba5d3839051dd9046
zer0_u <epsi10n7290@gmail.com>
2023-03-04 09:59:07 +0000

    Sat, Mar 4, 2023, 9:59 AM +00:00

--- 6_Python/pset/dna/dna.py
+++ 6_Python/pset/dna/dna.py
@@ -1,10 +1,15 @@
 from sys import argv
-
+import csv
 
 def main():
+    # コマンドライン引数をチェック
     if len(argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         return
-    print("111")
+    db_filename = argv[1]
+    sequence_filename=argv[2]
+
+    # データベースを読み込む
+
 
 main()
