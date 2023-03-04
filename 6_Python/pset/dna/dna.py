185b20c042a2fc5c07fd9b4fe0366d2e01793de6
zer0_u <epsi10n7290@gmail.com>
2023-03-04 11:13:23 +0000

    Sat, Mar 4, 2023, 11:13 AM +00:00

--- 6_Python/pset/dna/dna.py
+++ 6_Python/pset/dna/dna.py
@@ -1,24 +1,30 @@
 from sys import argv
 import csv
 
+
 def main():
     # コマンドライン引数をチェック
     if len(argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         return
+    # ファイル読み込み
     database = load_database(argv[1])
-
-
+    sequence = load_sequence(argv[2])
 
 
 def load_database(filename):
-    list =[]
-    with open(filename,"r") as file:
+    list = []
+    with open(filename, "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             list.append(row)
     return list
 
+
+def load_sequence(filename):
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        return next(reader)[0]
 
 
 main()
