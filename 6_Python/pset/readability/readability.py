5a11b6856f607c3694cfd8e837f1680b0a950cc3
zer0_u <epsi10n7290@gmail.com>
2023-03-04 08:38:26 +0000

    Sat, Mar 4, 2023, 8:38 AM +00:00

--- 6_Python/pset/readability/readability.py
+++ 6_Python/pset/readability/readability.py
@@ -9,7 +9,7 @@ def main():
     print(word_count)
     sentence_count = count_sentences(text)
     print(sentence_count)
-    grade = calculate_grade(letter_count,word_count,sentence_count)
+    grade = calculate_grade(letter_count, word_count, sentence_count)
     print(f"Grade: {grade}")
 
 
@@ -42,9 +42,13 @@ def count_sentences(text):
             count += 1
     return count
 
+
 def calculate_grade(letters, words, sentences):
     # L 100語あたりの平均文字数
+    l = (letters/words)*100
     # S 100語あたりの平均文章数
-    return 1
+    s = (sentences/words)*100
+    return 0.0588 * l - 0.296 * s - 15.8
+
 
 main()
