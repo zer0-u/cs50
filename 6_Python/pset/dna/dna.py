fd66e5c500f6426725c24376c5849f2f8c58806c
zer0_u <epsi10n7290@gmail.com>
2023-03-04 09:55:52 +0000

    Sat, Mar 4, 2023, 9:55 AM +00:00

--- 6_Python/pset/dna/dna.py
+++ 6_Python/pset/dna/dna.py
@@ -2,10 +2,9 @@ from sys import argv
 
 
 def main():
-    if len(argv) == 2:
-        print("aa")
-    else:
-        print("BB")
-
+    if len(argv) != 3:
+        print("Usage: python dna.py data.csv sequence.txt")
+        return
+    print("111")
 
 main()
