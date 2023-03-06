f5a0b792b55f4764d2a007bc031d7339ec8cb8e2
zer0_u <epsi10n7290@gmail.com>
2023-03-06 13:43:15 +0000

    Mon, Mar 6, 2023, 1:43 PM +00:00

--- 7_sql/lecture/favorites.py
+++ 7_sql/lecture/favorites.py
@@ -1,5 +1,7 @@
 import csv
 
+titles = set()
+
 with open("sample.csv", "r")as file:
     # reader = csv.reader(file)
     # next(reader)  # ひとまずヘッダは飛ばす
@@ -7,4 +9,7 @@ with open("sample.csv", "r")as file:
     #     print(row[1])  # タイトルだけ出力する
     reader = csv.DictReader(file)  # DictReaderを使うと堅牢性が増す
     for row in reader:
-        print(row["title"])
+        titles.add(row["title"])
+
+for title in sorted(titles):
+    print(title)
