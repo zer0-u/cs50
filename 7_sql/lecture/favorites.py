fff4fcdab24c0f0382d4416564543dd8d27e0ea2
zer0_u <epsi10n7290@gmail.com>
2023-03-06 13:34:22 +0000

    Mon, Mar 6, 2023, 1:34 PM +00:00

--- 7_sql/lecture/favorites.py
+++ 7_sql/lecture/favorites.py
@@ -1,7 +1,10 @@
 import csv
 
 with open("sample.csv", "r")as file:
-    reader = csv.reader(file)
-    next(reader)  # ひとまずヘッダは飛ばす
+    # reader = csv.reader(file)
+    # next(reader)  # ひとまずヘッダは飛ばす
+    # for row in reader:
+    #     print(row[1])  # タイトルだけ出力する
+    reader = csv.DictReader(file)  # DictReaderを使うと堅牢性が増す
     for row in reader:
-        print(row[1])  # タイトルだけ出力する
+        print(row["title"])
