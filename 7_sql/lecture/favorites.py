decbf624a56b7d635c3247f40624e95270a35de5
zer0_u <epsi10n7290@gmail.com>
2023-03-06 13:31:47 +0000

    Mon, Mar 6, 2023, 1:31 PM +00:00

--- 7_sql/lecture/favorites.py
+++ 7_sql/lecture/favorites.py
@@ -1,3 +1,7 @@
 import csv
 
-with open("sample.csv","r")
+with open("sample.csv", "r")as file:
+    reader = csv.reader(file)
+    next(reader)  # ひとまずヘッダは飛ばす
+    for row in reader:
+        print(row[1])  # タイトルだけ出力する
