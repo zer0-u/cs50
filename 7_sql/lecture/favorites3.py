b9493fe52062605f7aa83907732c450f5b5c5bd4
zer0_u <epsi10n7290@gmail.com>
2023-03-08 11:56:21 +0000

    Wed, Mar 8, 2023, 11:56 AM +00:00

--- 7_sql/lecture/favorites3.py
+++ 7_sql/lecture/favorites3.py
@@ -6,7 +6,10 @@ with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().upper()
-        
+        if title in titles:
+            titles[title] += 1
+        else:
+            titles[title] = 0
 
-for title in sorted(titles):
-    print(title)
+for title in titles:
+    print(title, titles[title])
