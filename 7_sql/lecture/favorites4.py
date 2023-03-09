15ec875400fdaa5af713d6fd95f2d1d0bcc85b46
zer0_u <epsi10n7290@gmail.com>
2023-03-09 11:40:03 +0000

    Thu, Mar 9, 2023, 11:40 AM +00:00

--- 7_sql/lecture/favorites4.py
+++ 7_sql/lecture/favorites4.py
@@ -1,8 +1,11 @@
 import csv
 
-titles = {}
-
+counter = 0
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().upper()
+        if title == "THE OFFICE":
+            counter += 1
+
+print(f"Number of people who like 'The office' : {counter}")
