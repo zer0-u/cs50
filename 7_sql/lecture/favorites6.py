6c6ec874c010b151b576b0b1bb25b97ba7408c49
zer0_u <epsi10n7290@gmail.com>
2023-03-13 12:42:31 +0000

    Mon, Mar 13, 2023, 12:42 PM +00:00

--- 7_sql/lecture/favorites6.py
+++ 7_sql/lecture/favorites6.py
@@ -9,9 +9,15 @@ title = input("title: ").strip()
 
 # SELECT文を実行する
 # ?はプレースホルダ
-# 戻り値は行のリスト
+# 戻り値は行のリスト(1行でもリスト)
 rows = db.execute("SELECT COUNT(*) FROM favorites WHERE title LIKE ?", title)
 
 # 結果を取り出す
 # 今回はCOUNT(*)だから1行目(0番目)にあると分かりきっている
 row = rows[0]
+
+print(row)
+
+# csv.readerと同じ感じで列名による参照も可
+# SELECT COUNT(*) AS COUNTER とかにしておくのもアリ
+print(row["COUNT(*)"])
