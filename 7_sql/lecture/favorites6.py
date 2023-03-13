b74429c2865d48215e02189db8d3eca431d566a1
zer0_u <epsi10n7290@gmail.com>
2023-03-13 12:37:31 +0000

    Mon, Mar 13, 2023, 12:37 PM +00:00

--- 7_sql/lecture/favorites6.py
+++ 7_sql/lecture/favorites6.py
@@ -7,4 +7,11 @@ db = SQL("sqlite:///favorites.db")
 # タイトルを入力してもらう
 title = input("title: ").strip()
 
+# SELECT文を実行する
+# ?はプレースホルダ
+# 戻り値は行のリスト
+rows = db.execute("SELECT COUNT(*) FROM favorites WHERE title LIKE ?", title)
 
+# 結果を取り出す
+# 今回はCOUNT(*)だから1行目(0番目)にあると分かりきっている
+row = rows[0]
