aafd02dfc0eb1f56a68bfd18f50c2ba37328c028
zer0_u <epsi10n7290@gmail.com>
2023-03-19 04:24:57 +0000

    Sun, Mar 19, 2023, 4:24 AM +00:00

--- 7_sql/pset/movies/12.sql
+++ 7_sql/pset/movies/12.sql
@@ -1,22 +1,28 @@
 -- Johnny DeppとHelena Bonham Carterの両方が主演したすべての映画のタイトルをリストする
-SELECT count(m.title) FROM movies m WHERE m.id IN
-    (SELECT s.movie_id FROM stars s WHERE s.person_id IN
-        (SELECT p.id FROM people p WHERE name = 'Johnny Depp' OR name = 'Helena Bonham Carter')
-    );
+-- 間違い
+-- SELECT count(m.title) FROM movies m WHERE m.id IN
+--     (SELECT s.movie_id FROM stars s WHERE s.person_id IN
+--         (SELECT p.id FROM people p WHERE name = 'Johnny Depp' OR name = 'Helena Bonham Carter')
+--     );
 -- Johnny Deppが出演したすべての映画のタイトル(61件)
-SELECT m.title FROM movies m WHERE m.id IN
-    (SELECT s.movie_id FROM stars s WHERE s.person_id =
-        (SELECT p.id FROM people p WHERE name = 'Johnny Depp')
-    );
+-- SELECT m.title FROM movies m WHERE m.id IN
+--     (SELECT s.movie_id FROM stars s WHERE s.person_id =
+--         (SELECT p.id FROM people p WHERE name = 'Johnny Depp')
+--     );
 -- Helena Bonham Carterが出演したすべての映画のタイトル(39件)
-SELECT m.title FROM movies m WHERE m.id IN
-    (SELECT s.movie_id FROM stars s WHERE s.person_id =
-        (SELECT p.id FROM people p WHERE name = 'Helena Bonham Carter')
-    );
+-- SELECT m.title FROM movies m WHERE m.id IN
+--     (SELECT s.movie_id FROM stars s WHERE s.person_id =
+--         (SELECT p.id FROM people p WHERE name = 'Helena Bonham Carter')
+--     );
 -- Johnnyが出演した中でHelenaが出演したもの
-SELECT m.title FROM movies m WHERE m.id IN
-    (SELECT m.id FROM movies m WHERE m.id IN
+SELECT m.title FROM movies m
+    WHERE m.id IN
         (SELECT s.movie_id FROM stars s WHERE s.person_id =
             (SELECT p.id FROM people p WHERE name = 'Johnny Depp')
         )
-    )
+    AND m.id IN
+        (SELECT s.movie_id FROM stars s WHERE s.person_id =
+            (SELECT p.id FROM people p WHERE name = 'Helena Bonham Carter')
+        )
+;
+
