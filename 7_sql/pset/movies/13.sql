9c33ffee6d886dcd391b3e1a9168f2cea69fdd79
zer0_u <epsi10n7290@gmail.com>
2023-03-19 03:35:51 +0000

    Sun, Mar 19, 2023, 3:35 AM +00:00

--- 7_sql/pset/movies/13.sql
+++ 7_sql/pset/movies/13.sql
@@ -1,1 +1,19 @@
 -- Kevin Baconが主演した映画に出演したすべての人の名前をリストする
+-- 1.kevin baconが出演した映画のIDを一覧する
+-- SELECT m.id FROM movies m WHERE m.id IN
+--     (SELECT s.movie_id FROM stars s WHERE s.person_id =
+--         (SELECT p.id FROM people p WHERE p.name = 'Kevin Bacon' AND p.birth = 1958)
+--     )
+-- ;
+-- ↑に出演したすべての人の名前をリストする
+SELECT p.name FROM people p WHERE p.id IN
+    (SELECT s.person_id FROM stars s WHERE s.movie_id IN
+        (SELECT m.id FROM movies m WHERE m.id IN
+            (SELECT s.movie_id FROM stars s WHERE s.person_id =
+                 (SELECT p.id FROM people p WHERE p.name = 'Kevin Bacon' AND p.birth = 1958)
+            )
+         )
+    )
+    AND p.id <> (SELECT p.id FROM people p WHERE p.name = 'Kevin Bacon' AND p.birth = 1958)
+ORDER BY p.name
+;
