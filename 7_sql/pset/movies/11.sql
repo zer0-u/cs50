a2247adc3dc2a207176fd7e9618af75a26a6a0c4
zer0_u <epsi10n7290@gmail.com>
2023-03-19 03:19:11 +0000

    Sun, Mar 19, 2023, 3:19 AM +00:00

--- 7_sql/pset/movies/11.sql
+++ 7_sql/pset/movies/11.sql
@@ -1,18 +1,9 @@
 -- Chadwick Bosemanが主演した映画の評価の高い順に5つのタイトルをリストする
-SELECT m.title. r.rating FROM movies m INNER JOIN ratings r
+SELECT m.title AS title FROM movies m INNER JOIN ratings r
     ON m.id = r.movie_id
     WHERE m.id IN
-    (SELECT s.movie_id FROM stars s WHERE s.person_id =
-        (SELECT p.id FROM people p WHERE p.name = 'Chadwick Boseman'
-        )) ORDER by r.rating;
-
-| 21 Bridges               |
-| 42                       |
-| Black Panther            |
-| Draft Day                |
-| Get on Up                |
-| Gods of Egypt            |
-| Ma Rainey's Black Bottom |
-| Marshall                 |
-| Message from the King    |
-| The Kill Hole            |
+        (SELECT s.movie_id FROM stars s WHERE s.person_id =
+            (SELECT p.id FROM people p WHERE p.name = 'Chadwick Boseman')
+        )
+    ORDER by r.rating DESC
+    LIMIT 5;
