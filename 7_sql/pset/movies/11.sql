e5cf082f13b9126a7a42efc0dd4898c5b8bcd023
zer0_u <epsi10n7290@gmail.com>
2023-03-19 03:12:42 +0000

    Sun, Mar 19, 2023, 3:12 AM +00:00

--- 7_sql/pset/movies/11.sql
+++ 7_sql/pset/movies/11.sql
@@ -1,2 +1,5 @@
 -- Chadwick Bosemanが主演した映画の評価の高い順に5つのタイトルをリストする
-SELECT m.title FROM movies m INNER JOIN people p ON m.
+SELECT m.title FROM movies m WHERE m.id IN
+    (SELECT s.movie_id FROM stars s WHERE s.person_id =
+        (SELECT p.id FROM people p WHERE p.name = 'Chadwick Boseman'
+        ));
