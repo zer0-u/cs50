19559e6ddf7fef61a466edcca87e62e8386cc219
zer0_u <epsi10n7290@gmail.com>
2023-03-19 03:16:35 +0000

    Sun, Mar 19, 2023, 3:16 AM +00:00

--- 7_sql/pset/movies/11.sql
+++ 7_sql/pset/movies/11.sql
@@ -1,8 +1,10 @@
 -- Chadwick Bosemanが主演した映画の評価の高い順に5つのタイトルをリストする
-SELECT m.title FROM movies m WHERE m.id IN
+SELECT m.title. r.rating FROM movies m INNER JOIN ratings r
+    ON m.id = r.movie_id
+    WHERE m.id IN
     (SELECT s.movie_id FROM stars s WHERE s.person_id =
         (SELECT p.id FROM people p WHERE p.name = 'Chadwick Boseman'
-        )) ORDER by m.title;
+        )) ORDER by r.rating;
 
 | 21 Bridges               |
 | 42                       |
