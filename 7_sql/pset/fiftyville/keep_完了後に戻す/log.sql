036383862a9c3845059fc9021d760a0acb97bfaf
zer0_u <epsi10n7290@gmail.com>
2023-03-19 07:16:42 +0000

    Sun, Mar 19, 2023, 7:16 AM +00:00

--- 7_sql/pset/fiftyville/keep_完了後に戻す/log.sql
+++ 7_sql/pset/fiftyville/keep_完了後に戻す/log.sql
@@ -21,10 +21,23 @@ select id,name,transcript from interviews
 ---- 情報C. (Raymond) 泥棒は裁判所から出る時誰かに短い(1分以内)の電話をかけた
 ---- 情報D. (Raymond) 泥棒は協力者に「明日(7/29)一番早くFiftyvilleを出る飛行機」のチケットを買うよう指示した
 
--- 調査3. 情報Aにもとづき、事件発生から10分以内に駐車場を出た車のナンバーを調べる
+-- 調査3-1. 情報Aにもとづき、事件発生から10分以内に駐車場を出た車のナンバーを調べる
 ---- 事件発生から10分以内 = 2020/07/28 午前10時15～25分
-select id, hour,minute, license_plate from courthouse_security_logs
-    where year = 2020 and month = 7 and day = 28 and hour = 10
+select id, hour, minute, license_plate from courthouse_security_logs
+    where year = 2020 and month = 7 and day = 28
+    and hour = 10
     and minute between 15 and 25
     and activity = 'exit'
     order by minute;
+
+-- 調査3-2. 調査3-1の結果を使って、車の持ち主の名前を調べる
+---- この時点での容疑者は8人
+select * from people
+    where license_plate in
+        (select license_plate from courthouse_security_logs
+            where year = 2020 and month = 7 and day = 28
+            and hour = 10
+            and minute between 15 and 25
+            and activity = 'exit'
+        )
+;
