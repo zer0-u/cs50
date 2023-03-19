4a95954feac9294d8dfb12bdbc3a4f044d5c6df7
zer0_u <epsi10n7290@gmail.com>
2023-03-19 07:13:51 +0000

    Sun, Mar 19, 2023, 7:13 AM +00:00

--- 7_sql/pset/fiftyville/3_security.sql
+++ 7_sql/pset/fiftyville/3_security.sql
@@ -1,4 +1,4 @@
-select id, minute, license_plate from courthouse_security_logs
+select id, hour,minute, license_plate from courthouse_security_logs
     where year = 2020 and month = 7 and day = 28 and hour = 10
     and minute between 10 and 25
     and activity = 'exit'
--- 7_sql/pset/fiftyville/keep_完了後に戻す/log.sql
+++ 7_sql/pset/fiftyville/keep_完了後に戻す/log.sql
@@ -16,7 +16,15 @@ select id,name,transcript from interviews
     and transcript like '%courthouse%';
 
 -- 証言から得られた情報
----- 情報1. (Ruth) 泥棒は事件から10分以内に裁判所の駐車場から出ていった
----- 情報2. (Eugene) 事件当日、泥棒はFifer StreetのATMで現金を引き出した
----- 情報3. (Raymond) 泥棒は裁判所から出る時誰かに短い(1分以内)の電話をかけた
----- 情報4. (Raymond) 泥棒は協力者に「明日(7/29)一番早くFiftyvilleを出る飛行機」のチケットを買うよう指示した
+---- 情報A. (Ruth) 泥棒は事件から10分以内に裁判所の駐車場から出ていった
+---- 情報B. (Eugene) 事件当日、泥棒はFifer StreetのATMで現金を引き出した
+---- 情報C. (Raymond) 泥棒は裁判所から出る時誰かに短い(1分以内)の電話をかけた
+---- 情報D. (Raymond) 泥棒は協力者に「明日(7/29)一番早くFiftyvilleを出る飛行機」のチケットを買うよう指示した
+
+-- 調査3. 情報Aにもとづき、事件発生から10分以内に駐車場を出た車のナンバーを調べる
+---- 事件発生から10分以内 = 2020/07/28 午前10時15～25分
+select id, hour,minute, license_plate from courthouse_security_logs
+    where year = 2020 and month = 7 and day = 28 and hour = 10
+    and minute between 15 and 25
+    and activity = 'exit'
+    order by minute;
