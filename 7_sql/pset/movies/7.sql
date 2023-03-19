bc7b8f0eaf769c6a929bdf4301b012c202152bbe
zer0_u <epsi10n7290@gmail.com>
2023-03-19 02:35:42 +0000

    Sun, Mar 19, 2023, 2:35 AM +00:00

--- 7_sql/pset/movies/7.sql
+++ 7_sql/pset/movies/7.sql
@@ -1,1 +1,2 @@
 -- 2010年にリリースされたすべての映画とそのレーティングをレーティングの降順でリストするSQLクエリを記述します。同じ評価の映画の場合は、タイトルのアルファベット順に並べ替えます
+SELECT m.title AS title, r.rating AS rating FROM movies m, ratings r INNER JOIN m.id = r.movie_id;
