328a805966e575a1d4ea8f4b351b30db3685889e
zer0_u <epsi10n7290@gmail.com>
2023-03-19 04:17:32 +0000

    Sun, Mar 19, 2023, 4:17 AM +00:00

--- 7_sql/pset/movies/12.sql
+++ 7_sql/pset/movies/12.sql
@@ -3,3 +3,13 @@ SELECT count(m.title) FROM movies m WHERE m.id IN
     (SELECT s.movie_id FROM stars s WHERE s.person_id IN
         (SELECT p.id FROM people p WHERE name = 'Johnny Depp' OR name = 'Helena Bonham Carter')
     );
+-- Johnny Deppが出演したすべての映画のタイトル(61件)
+SELECT m.title FROM movies m WHERE m.id IN
+    (SELECT s.movie_id FROM stars s WHERE s.person_id =
+        (SELECT p.id FROM people p WHERE name = 'Johnny Depp')
+    );
+-- Helena Bonham Carterが出演したすべての映画のタイトル(39件)
+SELECT m.title FROM movies m WHERE m.id IN
+    (SELECT s.movie_id FROM stars s WHERE s.person_id =
+        (SELECT p.id FROM people p WHERE name = 'Helena Bonham Carter')
+    );
