7b15ee3972804292234da77f96e4058eb4a9e4bb
zer0_u <epsi10n7290@gmail.com>
2023-03-19 03:13:26 +0000

    Sun, Mar 19, 2023, 3:13 AM +00:00

--- 7_sql/pset/movies/11.sql
+++ 7_sql/pset/movies/11.sql
@@ -3,3 +3,15 @@ SELECT m.title FROM movies m WHERE m.id IN
     (SELECT s.movie_id FROM stars s WHERE s.person_id =
         (SELECT p.id FROM people p WHERE p.name = 'Chadwick Boseman'
         ));
+
+| 42                       |
+| Message from the King    |
+| The Kill Hole            |
+| Black Panther            |
+| Draft Day                |
+| Gods of Egypt            |
+| Get on Up                |
+| Marshall                 |
+| 21 Bridges               |
+| Ma Rainey's Black Bottom |
++--------------------------+
