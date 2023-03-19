04777a16e4d86fe4ec652d989f57cddab745e23d
zer0_u <epsi10n7290@gmail.com>
2023-03-19 04:26:31 +0000

    Sun, Mar 19, 2023, 4:26 AM +00:00

--- 7_sql/pset/movies/10.sql
+++ 7_sql/pset/movies/10.sql
@@ -1,2 +1,2 @@
 -- 9.0以上のレーティングを受けた映画を監督したすべての人の名前をリストする
-SELECT count(p.name) FROM people p WHERE id IN (SELECT person_id FROM directors WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0));
+SELECT p.name FROM people p WHERE id IN (SELECT person_id FROM directors WHERE movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0));
