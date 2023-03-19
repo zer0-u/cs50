a8f01b24aa569d66ff4d971bc59affa467eacb44
zer0_u <epsi10n7290@gmail.com>
2023-03-19 03:22:35 +0000

    Sun, Mar 19, 2023, 3:22 AM +00:00

---
+++ 7_sql/pset/movies/12.sql
@@ -0,0 +1,5 @@
+-- Johnny DeppとHelena Bonham Carterの両方が主演したすべての映画のタイトルをリストする
+SELECT m.title FROM movies m WHERE m.id IN
+    (SELECT s.movie_id FROM stars s WHERE s.person_id IN
+        (SELECT p.id FROM people p WHERE name = 'Johnny Depp' OR name = 'Helena Bonham Carter')
+    );
