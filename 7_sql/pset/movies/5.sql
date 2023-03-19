1aa0b4fc70f286d3e26cfa840e15e800a69a6970
zer0_u <epsi10n7290@gmail.com>
2023-03-19 02:28:06 +0000

    Sun, Mar 19, 2023, 2:28 AM +00:00

--- 7_sql/pset/movies/5.sql
+++ 7_sql/pset/movies/5.sql
@@ -1,2 +1,4 @@
 -- すべてのハリー・ポッター映画のタイトルと公開年を時系列でリストする
+-- 映画のタイトルが 「Harry Potter」 という単語で始まる場合、それはハリー・ポッター映画であると判別できます。
+-- 「含む」ではないらしい
 SELECT title, year FROM movies WHERE title LIKE 'Harry Potter%' ORDER BY year;
