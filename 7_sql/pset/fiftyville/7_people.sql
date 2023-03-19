3bf0fb34abcced888c92a850b51c688998f873c5
zer0_u <epsi10n7290@gmail.com>
2023-03-19 06:30:30 +0000

    Sun, Mar 19, 2023, 6:30 AM +00:00

--- 7_sql/pset/fiftyville/7_people.sql
+++ 7_sql/pset/fiftyville/7_people.sql
@@ -1,3 +1,7 @@
+-- 泥棒を探す
+-- ヒント1 監視カメラから分かったナンバープレート
+-- ヒント2 事件翌日にヒースローへ移動した乗客のパスポート番号
+-- ヒント3 逃走直前にかけていた電話の記録
 select * from people
     where license_plate in
         (select license_plate from courthouse_security_logs
@@ -11,5 +15,14 @@ select * from people
         (select caller from phone_calls
             where year = 2020 and month = 7 and day = 28
             and duration < 60
+        )
+;
+
+-- 協力者を探す
+select * from people
+    where phone_number in
+        (select receiver from phone_calls
+            where year = 2020 and month = 7 and day = 28
+            and caller in ('(130) 555-0289','(499) 555-9472','(367) 555-5533')
         )
 ;
