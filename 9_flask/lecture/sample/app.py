ee98a56baeb17ae04e255b97c7f04475e436e4fc
zer0_u <epsi10n7290@gmail.com>
2023-04-14 12:37:46 +0000

    Fri, Apr 14, 2023, 12:37 PM +00:00

--- 9_flask/lecture/sample/app.py
+++ 9_flask/lecture/sample/app.py
@@ -5,10 +5,10 @@ from flask import Flask, render_template, request
 # Flask関数で現在のファイルをFlaskアプリケーションに変換し、変数appに代入する
 app = Flask(__name__)
 
-# 引数のないindex関数でindex.htmlのレンダリング結果を返す
+# 引数のないindex関数でindex.htmlのレンダ・テンプレートを返す
 # index.htmlはこれから作る(どこかにある想定)
 # @から始まる文字列は「デコレータ」と呼ぶ(アノテーションではない)
-# /(アプリケーションルート)
+# /(アプリケーションルート)にアクセスしたときの挙動を定義
 @app.route("/")
 def index():
     return render_template("index.html")
