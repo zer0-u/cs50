fc6f1694a5ca8224b80931abb8568adb00de335d
zer0_u <epsi10n7290@gmail.com>
2023-04-14 12:33:37 +0000

    Fri, Apr 14, 2023, 12:33 PM +00:00

--- 9_flask/lecture/app.py
+++ 9_flask/lecture/app.py
@@ -1,6 +1,11 @@
-// 使うものをimportする
+# 使うものをimportする
 from flask import Flask, render_template, request
 
-// __name__は現在のファイル名を差している
-// Flask関数で現在のファイルをFlaskアプリケーションに変換し、変数appに代入する
+# __name__は現在のファイル名を差している
+# Flask関数で現在のファイルをFlaskアプリケーションに変換し、変数appに代入する
 app = Flask(__name__)
+
+# 引数のない
+#
+def index():
+    return render_template("index.html")
