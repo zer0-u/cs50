ad226f543580d6276b7a5b3c6fbe3bc1fa40d9b5
zer0_u <epsi10n7290@gmail.com>
2023-04-17 12:51:37 +0000

    Mon, Apr 17, 2023, 12:51 PM +00:00

--- 9_flask/lecture/froshims/app.py
+++ 9_flask/lecture/froshims/app.py
@@ -3,15 +3,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "basketball",
+    "foorball",
+    "soccer",
+    "ultimate frisbee"
+]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 
 @app.route("/register", methods=["POST"])
 def register():
     # 入力値チェック
-    if not request.form.get("name") or request.form.get("sport") not in ["basketball", "soccer", "ultimate frisbee"]:
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     return render_template("success.html")
