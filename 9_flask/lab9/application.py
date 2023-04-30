ce8af3c6c29d9e26061f51f9e730ef4b1931a73b
zer0_u <epsi10n7290@gmail.com>
2023-04-30 05:27:45 +0000

    Sun, Apr 30, 2023, 5:27 AM +00:00

--- 9_flask/lab9/application.py
+++ 9_flask/lab9/application.py
@@ -13,21 +13,18 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 db = SQL("sqlite:///birthdays.db")
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
+@app.route("/", methods=["GET"])
+def index_get():
+    birthdays = db.execute("SELECT * FROM birthdays")
+    return render_template("index.html", birthdays=birthdays)
 
-        # TODO: Add the user's entry into the database
-        name = request.form.get("name")
-        month = request.form.get("month")
-        day = request.form.get("day")
-        db.execute(
-            "INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)",
-            name, month, day)
-        return redirect("/")
 
-    else:
-
-        # TODO: Display the entries in the database on index.html
-        birthdays = db.execute("SELECT * FROM birthdays")
-        return render_template("index.html", birthdays=birthdays)
+@app.route("/", methods=["POST"])
+def index_post():
+    name = request.form.get("name")
+    month = request.form.get("month")
+    day = request.form.get("day")
+    db.execute(
+        "INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)",
+        name, month, day)
+    return redirect("/")
