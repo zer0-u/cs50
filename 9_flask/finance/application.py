87675a6f5f3c3af2fe52317b7a726437cd53d0c0
zer0_u <epsi10n7290@gmail.com>
2023-05-01 11:56:56 +0000

    Mon, May 1, 2023, 11:56 AM +00:00

--- 9_flask/finance/application.py
+++ 9_flask/finance/application.py
@@ -118,14 +118,11 @@ def quote():
     return apology("TODO")
 
 
-@app.route("/register", methods=["GET"])
-def register_get():
-    """show registration page"""
-    return render_template("register.html")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
 
-
-@app.route("/register", methods=["POST"])
-def register_post():
     """Register user"""
     # 入力値を取得する
     name = request.form.get("username")
