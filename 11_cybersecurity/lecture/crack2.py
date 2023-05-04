534c0cba8dec3083e0f967463c00c5ae80aec27f
zer0_u <epsi10n7290@gmail.com>
2023-05-04 09:35:48 +0000

    Thu, May 4, 2023, 9:35 AM +00:00

--- 11_cybersecurity/lecture/crack2.py
+++ 11_cybersecurity/lecture/crack2.py
@@ -1,5 +1,5 @@
 from string import digits, ascii_letters, punctuation  # 句読点
 from itertools import product  # たくさんの数の直積を計算する関数
 
-for passcode in product(digits, repeat=4):
+for passcode in product(ascii_letters + digits + punctuation, repeat=8):
     print(*passcode)
